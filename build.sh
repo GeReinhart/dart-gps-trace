c5485a9b277c02a878a0e6d11ff6b390966bac55
Drone Server <support@drone.io>
2014-01-11 23:10:34 +0100

    build

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function get-dart-dependencies {
+	echo "get-dart-dependencies"
+	pub get || { echo 'get-dart-dependencies failed' ; exit 1; }
+}
+
+function test-dart-sources {
+	echo "test-dart-sources"
+	dart test/model_tests.dart || { echo 'test-dart-sources failed' ; exit 1; }
+}
+
+
+get-dart-dependencies 
+test-dart-sources
+
+
